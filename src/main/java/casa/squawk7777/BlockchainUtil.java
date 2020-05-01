3657ae384255519f5b8c6908476e7549841de173
Mikhail Vyshinskiy <mg.vyshinskiy@gmail.com>
2020-05-01 23:29:58 +0300

    Dropped support of saving/loading blockchain from file; minor fixes

--- src/main/java/casa/squawk7777/BlockchainUtil.java
+++ src/main/java/casa/squawk7777/BlockchainUtil.java
@@ -4,12 +4,6 @@ import casa.squawk7777.exceptions.InvalidBlockException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.concurrent.ThreadLocalRandom;
@@ -18,28 +12,7 @@ public class BlockchainUtil {
     private static final Logger log = LoggerFactory.getLogger(BlockchainUtil.class);
     private BlockchainUtil() {}
 
-    public static Blockchain loadBlockchain(File blockchainFile) {
-        log.debug("Loading blockchain from file: {}", blockchainFile);
-        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(blockchainFile))) {
-            Blockchain blockchain = (Blockchain) inputStream.readObject();
-            log.debug("Successfully loaded blockchain containing {} blocks", blockchain.getSize());
-            return blockchain;
-        } catch (IOException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static void saveBlockchain(File blockchainFile, Blockchain blockchain) {
-        log.debug("Saving blockchain to file: {}", blockchainFile);
-        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(blockchainFile))) {
-            outputStream.writeObject(blockchain);
-            log.debug("Blockchain containing {} blocks successfully saved", blockchain.getSize());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static Block generateBlock(Blockchain.Challenge challenge, String miner) throws InvalidBlockException {
+    public static <T extends WorkloadItem> Block<T> generateBlock(Blockchain<T>.Challenge<T> challenge, String miner) throws InvalidBlockException {
         log.debug("Seeking hash for block with {} messages starting with:\n{} (previous block hash: {})",
                 challenge.getWorkload().size(), challenge.getSeekingString(), challenge.getLastHash());
 
@@ -55,7 +28,7 @@ public class BlockchainUtil {
         }
         log.debug("Found appropriate hash\n({}) with salt: {}", currentHash, salt);
 
-        return new Block(challenge.getId(), challenge.getComplexity(), salt, currentHash, miner, challenge.getWorkload());
+        return new Block<>(challenge.getId(), challenge.getComplexity(), salt, currentHash, miner, challenge.getWorkload());
     }
 
     public static String calculateHash(String input) {
